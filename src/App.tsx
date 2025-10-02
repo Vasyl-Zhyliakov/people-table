ce6ec1c631183d032d4997143022618ccaba2cd9
Vasyl Zhyliakov <snak87@gmail.com>
2025-10-02 14:34:45 +0300

    first fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import { PeoplePage } from './components/PeoplePage';
+import { Navbar } from './components/Navbar';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div data-cy="app">
+      <Navbar />
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="/home" element={<Navigate to="/" replace={true} />} />
+            <Route path="/people">
+              <Route path=":personSlug?" element={<PeoplePage />} />
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 };
